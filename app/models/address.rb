928dddfa8a838285639ba104ec21bfa1457233f9
Chad Chaney <cchane4@gmail.com>
2018-03-05 21:28:27 -0600

    adding validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
+  validates :state, length: {maximum: 2}
+  validates :street, :city, :state, :zip, presence: true
 end
