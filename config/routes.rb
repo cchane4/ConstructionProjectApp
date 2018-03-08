c7326e4829c632cc9af13d217d6aa52e1dabe262
Chad Chaney <cchane4@gmail.com>
2018-03-07 19:44:26 -0600

    add sign in flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'sessions#index'
+get :signin, to: 'sessions#new', as: :signin
+resources :sessions, only: :create
+delete :signout, to: 'session#destroy', as: :signout
+
+get :signup, to: 'user#show', as: :signup
+
+resources :projects
 end
