1a9033cd0935dbcc7a84867e5e57c05365b94519
Chad Chaney <cchane4@gmail.com>
2018-03-11 21:46:59 -0500

    add gravatar for users photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def gravatar_for(email)
+    hashed_email = Disgest::MD5.hexdigest(email.downcast.strip)
+    image_tag "http://www.gravatar.com/avatar/#{hashed_email}?s=50.jpg", alt: "Profile Photo"
+  end
 end
