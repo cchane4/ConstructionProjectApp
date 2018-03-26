84cb5e669be1ab2eea9207df66d0a7b6f99386c3
Chad Chaney <cchane4@gmail.com>
2018-03-26 14:50:33 -0500

    making more corrections

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,9 +1,9 @@
 class Weather
-  def self.get_weather(_zip)
+  def self.get_weather(zip)
     response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{zip},us&APPID=#{ENV['WEATHER_KEY']}")
     parsed_response = response.parsed_response
-    description = parsed_response['weather'][0]["description"]
-    temp = to_farenheit(parsed_response['main']['temp'])
+    description = parsed_response["weather"][0]["description"]
+    temp = to_farenheit(parsed_response["main"]["temp"])
     { description: description,
       temp: temp,
       image: image(description)}
@@ -12,7 +12,7 @@ class Weather
   end
 
   def self.to_farenheit(kelvin)
-    (kelvin * 9 / 5 - 459.67).round.to_s + '°';
+    (kelvin*9/5 - 459.67).round.to_s + '°';
   end
 
   def self.image(description)
