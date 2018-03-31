4c0e8fe5b2cbec6f062fe3d9c3983bd4432400b4
Chad Chaney <cchane4@gmail.com>
2018-03-31 09:52:58 -0500

    added test for user models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  fixtures :users
+  let!(:user) { user(:user)
+  }
+
+  describe ".validates" do
+    context "when email is nil" do
+      it "is invalid" do
+        user.update(email: nil)
+        expect(user.errors[:email]).to include "can't be blank"
+    end
+  end
+
+  context "when username is nil" do
+    it "is invalid" do
+      user.update(username: nil)
+      expect(user.errors[:username]).to include "cant be blank"
+    end
+  end
+
+  it "is invalid if email does not have @" do
+    user.update(email: 'test.com')
+    expect(user.errors[:email]).to include "is invalid"
+    end
+  end
+end
+
+
+
